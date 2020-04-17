f41c5e089bbc613466108c070ddccb7c93bb654b
Abdullah <aharis@utexas.edu>
2020-04-17 14:19:48 -0500

    Implemented checkHandForBook

--- player.cpp
+++ player.cpp
@@ -24,11 +24,36 @@ void Player::bookCards(Card c1, Card c2) {//moves c1 and c2 from hand to books
     removeCardFromHand(c2);
 }
 
+// Compares two cards. Sorts by rank, then string representation.
+bool cardCmp(Card& c1, Card& c2){
+    if (c1.getRank() < c2.getRank()){
+        return true;
+
+    } else if (c1.getRank() == c2.getRank()){
+        return c1.toString() < c2.toString();
+    }
+    return false;
+}
+
 //OPTIONAL
 // comment out if you decide to not use it
 //this function will check a players hand for a pair.
 //If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
 bool Player::checkHandForBook(Card& c1, Card& c2) {
+    if (myHand.size() <= 1){
+        return false;
+    }
+
+    sort(myHand.begin(), myHand.end(), cardCmp); //All books are now side by side.
+
+    for(int i = 1; i < myHand.size(); i++){
+        if(myHand[i-1].getRank() == myHand[i].getRank()){
+            c1 = myHand[i-1];
+            c2 = myHand[i];
+            return true;
+        }
+    }
+
     return false;
 }
 
